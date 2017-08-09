70f7b0b01bc81990887836d0a604ab9791d4b670
ZhangKong <602488225@qq.com>
2017-08-09 20:38:16 +0800

    初始化

---
+++ src/VipAnalyser.Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VipAnalyser.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://v.qq.com/u/history/";
+
+            PhantomJSHelper.Instance.GoToUrl(url);
+
+            if (PhantomJSHelper.Instance.WaitForElementExists(By.Id("web_qr_login"), 10))
+            {
+
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
